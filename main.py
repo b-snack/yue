c1ed44366b6e531c40fa4e2d88d0b53336a42f71
bsnack <swuott2009@gmail.com>
2026-06-30 19:33:40 -0400

    added plotting the waveform on a graph using np.arange and plt.plot. code works up to now

--- main.py
+++ main.py
@@ -10,3 +10,13 @@ y, sr = librosa.load(filename)
 # so, at each instance it takes the amplitude and stores it in an array
 print(f"loaded {len(y)} samples at {sr} Hz")
 print(f"{len(y) / sr:.5f} seconds")
+
+# np.arange(leny) / sr bc $\frac{samples}{\frac{samples}{second}}$ = seconds
+time = np.arange(len(y)) / sr
+
+plt.figure(figsize=(10,4))
+plt.plot(time, y)
+plt.xlabel("Time (s)")
+plt.ylabel("Waveform amp / time")
+plt.grid(True)
+plt.show()
