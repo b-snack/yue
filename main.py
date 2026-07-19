4cc5a8fd9131757dbaae9c444e2d026b6f8a303e
bsnack <swuott2009@gmail.com>
2026-07-18 20:04:02 -0400

    fixed structure/order of main.py

--- main.py
+++ main.py
@@ -3,76 +3,37 @@ import numpy as np
 import matplotlib.pyplot as plt
 from notes import piano_notes
 
+# ------------------- CONSTANTS -------------------
+
 CHUNK = 50
 SPEED_OF_SOUND = 340 # m \cdot s^(-1)
 
-filename = "sample.wav"
+# -------------------- FUNCTIONS ---------------------
+def load_audio(filename):
+  sr, y = wavfile.read(filename)
 
-sr, y = wavfile.read(filename)
+  if y.dtype != np.float32:
+    y=y.astype(np.float32) / np.iinfo(y.dtype).max
 
-if y.dtype != np.float32:
-  y=y.astype(np.float32) / np.iinfo(y.dtype).max
+  if len(y.shape) > 1:
+    y=np.mean(y, axis=1)
 
-if len(y.shape) > 1:
-  y=np.mean(y, axis=1)
+  # y is amplitude of wave, sr is frequency of sampling
+  # so, at each instance it takes the amplitude and stores it in an array
+  print(f"loaded {len(y)} samples at {sr} Hz")
+  print(f"{len(y) / sr:.5f} seconds")
 
-# y is amplitude of wave, sr is frequency of sampling
-# so, at each instance it takes the amplitude and stores it in an array
-print(f"loaded {len(y)} samples at {sr} Hz")
-print(f"{len(y) / sr:.5f} seconds")
+  return y, sr
 
-# np.arange(leny) / sr bc $\frac{samples}{\frac{samples}{second}}$ = seconds
-time = np.arange(len(y)) / sr
+def find_peaks(amp):
+  peaks = []
+  for i in range(1, len(amp)-1):
+    if amp[i] > amp[i-1] and amp[i] > amp[i+1] and amp[i] > 500:
+      peaks.append(i)
 
-plt.figure(figsize=(10,4))
-plt.plot(time, y)
-plt.xlabel("Time (s)")
-plt.ylabel("Waveform amp / time")
-plt.grid(True)
-plt.show()
-
-y_fft = np.fft.rfft(y)
-# nps.abs strips the phase and takes the magnitude bc phase is not needed. 
-
-amp = np.abs(y_fft)
-
-frequencies = np.fft.rfftfreq(len(y), d=1.0/sr)
-
-plt.figure(figsize=(10,4))
-plt.plot(frequencies, amp, color='r')
-plt.title("Freq spectrum")
-plt.xlabel("Frequency")
-plt.ylabel("Magnitude")
-plt.grid(True)
-plt.show()
-
-# np.argmax() returns the index of the largest value of an array; the index by itself isn't useful but
-# since freq & mangitude are same length and addup, the index can look up the frequency
-
-peak_index = np.argmax(amp)
-peak_freq = frequencies[peak_index] # freq of loudest bin
-
-most_accurate_note = [abs(peak_freq - piano_notes[1]["fundamental_hz"]), piano_notes[1]["note"]]
-
-print(most_accurate_note[1])
-
-chunk_size = int(sr // (1000 // CHUNK))
-
-for i in range(0, len(y), chunk_size):
-
-  y_fft = np.fft.rfft(y[i: i+chunk_size])
-  # nps.abs strips the phase and takes the magnitude bc phase is not needed. 
-
-  amp = np.abs(y_fft)
-
-  frequencies = np.fft.rfftfreq(chunk_size, d=1.0/sr)
-
-  # np.argmax() returns the index of the largest value of an array; the index by itself isn't useful but
-  # since freq & mangitude are same length and addup, the index can look up the frequency
-
-  peak_index = np.argmax(amp)
-  peak_freq = frequencies[peak_index] # freq of loudest bin
-
+  return peaks
+  
+def match_note(peak_freq):
   most_accurate_note = [abs(peak_freq - piano_notes[1]["fundamental_hz"]), piano_notes[1]["note"]]
   for _ in piano_notes:
     fundamental = piano_notes[_]["fundamental_hz"]
@@ -80,12 +41,61 @@ for i in range(0, len(y), chunk_size):
     if difference <= most_accurate_note[0]:
       most_accurate_note = [difference, piano_notes[_]["note"]]
   
-  print(f"{most_accurate_note[1]} from {i / sr} to {(i + chunk_size) / sr} seconds")
-  print(f"peak frequency = {peak_freq}")
+  return most_accurate_note[1]
+    
+def main():
+  y, sr = load_audio("sample.wav")
 
-def find_peaks(amp):
-  peaks = []
-  for i in range(1, len(amp)-1):
-    if amp[i] > amp[i-1] and amp[i] > amp[i+1] and amp[i] > 500:
-      peaks.append(i)
-  
+  # np.arange(leny) / sr bc $\frac{samples}{\frac{samples}{second}}$ = seconds
+  time = np.arange(len(y)) / sr
+
+
+  y_fft = np.fft.rfft(y)
+  # nps.abs strips the phase and takes the magnitude bc phase is not needed. 
+
+  amp = np.abs(y_fft)
+
+  frequencies = np.fft.rfftfreq(len(y), d=1.0/sr)
+
+  # np.argmax() returns the index of the largest value of an array; the index by itself isn't useful but
+  # since freq & mangitude are same length and addup, the index can look up the frequency
+  peak_indices = find_peaks(amp)
+  peak_freqs = frequencies[peak_indices]
+  peak_amps = amp[peak_indices] # freq of loudest bin
+
+  plt.figure(figsize=(10,4))
+  plt.plot(frequencies, amp, color='r', label='Spectrum')
+
+  plt.scatter(peak_freqs, peak_amps, color='blue', zorder=3, label='Peaks')
+
+  plt.xlabel("frequency (Hz)")
+  plt.ylabel("Magnitude")
+  plt.grid(True)
+  plt.legend()
+  plt.show()
+
+  chunk_size = int(sr // (1000 // CHUNK))
+
+  for i in range(0, len(y), chunk_size):
+
+    chunk_data = y[i: i+chunk_size]
+
+    # nps.abs strips the phase and takes the magnitude bc phase is not needed. 
+
+    y_fft_chunk = np.fft.rfft(chunk_data)
+    chunk_amp = np.abs(y_fft_chunk)
+    frequencies_chunk = np.fft.rfftfreq(chunk_size, d=1.0/sr)
+
+    # np.argmax() returns the index of the largest value of an array; the index by itself isn't useful but
+    # since freq & mangitude are same length and addup, the index can look up the frequency
+
+    peak_index_chunk = np.argmax(chunk_amp)
+    peak_freq_chunk = frequencies_chunk[peak_index_chunk] # freq of loudest bin
+
+    current_note_name = match_note(peak_freq_chunk)
+
+    print(f'{current_note_name} from {i / sr:.3f} to {(i+chunk_size)/ sr:.3f} seconds')
+    print(f'peak frequency = {peak_freq_chunk:.2f} Hz')
+
+if __name__ == "__main__":
+  main()
