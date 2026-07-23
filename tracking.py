049eb47bb7be34540acc2d8f6c888592af858d1e
bsnack <swuott2009@gmail.com>
2026-07-22 21:12:00 -0400

    shuold be working? python code to integrate into main.py

--- tracking.py
+++ tracking.py
@@ -1,4 +1,4 @@
-
+import numpy as np
 """
 before loop:
   current note = none
@@ -25,3 +25,27 @@ print(notes)
 if unclear and then reverts back to c4 we'll just assume that its a held note. 
 
 """
+CHUNK = 200
+notes = []
+current_note = None
+start_time = 0
+sr = 44100
+chunk_size = int(sr // (1000 // CHUNK))
+time = np.arange(len(y)) / sr
+note_name = None
+
+for note in (time//(chunk_size/1000)-1):
+  if note_name == "unclear":
+    pass
+  elif note_name == current_note:
+    pass
+  else:
+    if current_note is not None:
+      notes.append([current_note, start_time, i/sr])
+    current_note = note_name
+    start_time = i/sr
+
+if current_note is not None:
+  notes.append([current_note, start_time, time])
+
+print(notes)
